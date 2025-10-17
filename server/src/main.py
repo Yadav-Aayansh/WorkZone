c62d5d2af3bfcc1b814aa2b67daed4817acce528
Yadav Aayansh <aayanshyadav@proton.me>
2025-10-18 01:29:24 +0530

    Enabled cors

--- server/src/main.py
+++ server/src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from src.core.database import init_db
 from src.routes.platform import platform_router
-
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,5 +10,13 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Noctivagous!! 
+    allow_credentials=True,
+    allow_methods=["*"],  # Noctivagous!!
+    allow_headers=["*"],  # Noctivagous!!
+)
 
 app.include_router(platform_router)
