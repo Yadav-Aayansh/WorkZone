65c0ad093e7f4869beeeb2783c6250786216bc34
Yadav Aayansh <aayanshyadav@proton.me>
2025-11-09 15:41:24 +0530

    Fix: Config issue

--- server/src/core/config.py
+++ server/src/core/config.py
@@ -2,34 +2,42 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from pathlib import Path
 
 class Settings(BaseSettings):
-    # FRONTEND_URL: str
-    # LOG_LEVEL: str
+    FRONTEND_URL: str
+    LOG_LEVEL: str
 
-    # # Database
-    # SYNC_DATABASE_URL: str
-    # ASYNC_DATABASE_URL: str
+    # Database
+    SYNC_DATABASE_URL: str
+    ASYNC_DATABASE_URL: str
 
+    # Google Cloud
+    GOOGLE_PROJECT_ID: str
+    GOOGLE_PRIVATE_KEY: str
+    GOOGLE_CLIENT_EMAIL: str
+    GCS_BUCKET_NAME: str
 
-    # # JWT
-    # JWT_SECRET_KEY: str
-    # JWT_ALGORITHM: str
+    # Gemini API
+    GOOGLE_API_KEY: str
 
-    # DOMAIN_NAME: str
+    # JWT
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
 
-    # # Razorpay
-    # RAZORPAY_KEY_ID: str
-    # RAZORPAY_KEY_SECRET: str
-    # RAZORPAY_WEBHOOK_SECRET: str
+    DOMAIN_NAME: str
 
-    # # Background Tasks
-    # REDIS_URL: str
+    # Razorpay
+    RAZORPAY_KEY_ID: str
+    RAZORPAY_KEY_SECRET: str
+    RAZORPAY_WEBHOOK_SECRET: str
 
-    # # SMTP
-    # SMTP_HOST: str
-    # SMTP_PORT: int
-    # SMTP_NAME: str
-    # SMTP_USER: str
-    # SMTP_PASSWORD: str
+    # Background Tasks
+    REDIS_URL: str
+
+    # SMTP
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_NAME: str
+    SMTP_USER: str
+    SMTP_PASSWORD: str
 
     # Class Variable
     model_config = SettingsConfigDict(
