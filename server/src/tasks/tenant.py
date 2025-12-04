ab568b7f2a4cd27161e565f1749bbc75e8e92a39
Yadav Aayansh <aayanshyadav@proton.me>
2025-12-04 16:04:24 +0530

    Added email tasks and fixed other tasks

--- server/src/tasks/tenant.py
+++ server/src/tasks/tenant.py
@@ -1,11 +1,13 @@
 from src.core.celery import worker
+from .email import send_onboarding_email_task
 from src.core.database import create_tenant_schema
 from src.core.database import get_public_db_sync
 from src.models.platform import Setting
 
 @worker.task(bind=True, max_retries=3)
-def create_tenant_schema_task(self, tenant_id: str):
+def create_tenant_schema_task(self, tenant_id: str, name: str, email: str, brand: str, dashboard_link: str):
     create_tenant_schema(tenant_id)
+    send_onboarding_email_task.delay(email, name, brand, tenant_id, dashboard_link)
 
 @worker.task(bind=True, max_retries=3)
 def create_tenant_setting(self, client_id: str):
